dfc9b98426572db586fbc589220a2586e4feabb9
Nick Rokosz <nicholasrokosz@gmail.com>
2023-01-09 10:40:44 -0700

    Format packer.lua

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -3,7 +3,7 @@
 -- Current workflow for adding a new plugin: `:w`, `:so`, `:PackerSync`
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 vim.cmd([[
   augroup packer_user_config
@@ -14,151 +14,157 @@ vim.cmd([[
 
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-    return
+	return
 end
 
 packer.init({
-    display = {
-        open_fn = function()
-            return require("packer.util").float({ border = 'none' })
-        end,
-    },
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "none" })
+		end,
+	},
 })
 
 return packer.startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    use { "nvim-telescope/telescope-file-browser.nvim" }
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
 
-    use {
-        "nvim-telescope/telescope-frecency.nvim",
-        config = function()
-            require "telescope".load_extension("frecency")
-        end,
-        requires = { "kkharji/sqlite.lua" }
-    }
+	use({
+		"nvim-telescope/telescope-frecency.nvim",
+		config = function()
+			require("telescope").load_extension("frecency")
+		end,
+		requires = { "kkharji/sqlite.lua" },
+	})
 
-    use {
-        "mcchrish/zenbones.nvim",
-        -- Optionally install Lush. Allows for more configuration or extending the colorscheme
-        -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
-        -- In Vim, compat mode is turned on as Lush only works in Neovim.
-        requires = "rktjmp/lush.nvim"
-    }
+	use({
+		"mcchrish/zenbones.nvim",
+		-- Optionally install Lush. Allows for more configuration or extending the colorscheme
+		-- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+		-- In Vim, compat mode is turned on as Lush only works in Neovim.
+		requires = "rktjmp/lush.nvim",
+	})
 
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    use({ -- Additional text objects via treesitter
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        after = "nvim-treesitter",
-    })
+	use({ -- Additional text objects via treesitter
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		after = "nvim-treesitter",
+	})
 
-    use('mbbill/undotree')
+	use("mbbill/undotree")
 
-    -- taken from https://github.com/VonHeikemen/lsp-zero.nvim
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+	-- taken from https://github.com/VonHeikemen/lsp-zero.nvim
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            -- Snippet Collection (Optional)
-            { 'rafamadriz/friendly-snippets' },
-        }
-    }
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			-- Snippet Collection (Optional)
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
 
-    use "folke/neodev.nvim"
+	use("folke/neodev.nvim")
 
-    use 'j-hui/fidget.nvim'
+	use("j-hui/fidget.nvim")
 
-    use 'kyazdani42/nvim-web-devicons'
+	use("kyazdani42/nvim-web-devicons")
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-        config = function()
-            require('lualine').setup({})
-        end
-    }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+		config = function()
+			require("lualine").setup({})
+		end,
+	})
 
-    use 'lewis6991/impatient.nvim'
+	use("lewis6991/impatient.nvim")
 
-    use 'karb94/neoscroll.nvim'
+	use("karb94/neoscroll.nvim")
 
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 
-    use "lewis6991/gitsigns.nvim"
+	use("lewis6991/gitsigns.nvim")
 
-    use "lukas-reineke/indent-blankline.nvim"
+	use("lukas-reineke/indent-blankline.nvim")
 
-    use {
-        "numToStr/Comment.nvim",
-        config = function() require("Comment").setup() end
-    }
+	use({
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	})
 
-    use 'goolord/alpha-nvim'
+	use("goolord/alpha-nvim")
 
-    use {
-        'ggandor/leap.nvim',
-        config = function()
-            require('leap').set_default_keymaps()
-        end
-    }
+	use({
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").set_default_keymaps()
+		end,
+	})
 
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons',
-        }
-    }
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+		},
+	})
 
-    use {
-        'romgrk/barbar.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons' }
-    }
+	use({
+		"romgrk/barbar.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
 
-    use {
-        "akinsho/toggleterm.nvim",
-        tag = '*'
-    }
+	use({
+		"akinsho/toggleterm.nvim",
+		tag = "*",
+	})
 
-    use {
-        "folke/trouble.nvim",
-        requires = "kyazdani42/nvim-web-devicons",
-        config = function()
-            require("trouble").setup()
-        end
-    }
+	use({
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup()
+		end,
+	})
 
-    use 'folke/which-key.nvim'
+	use("folke/which-key.nvim")
 
-    use { 'michaelb/sniprun', run = 'bash ./install.sh' }
+	use({ "michaelb/sniprun", run = "bash ./install.sh" })
 
-    use({
-        "jose-elias-alvarez/null-ls.nvim",
-        "jayp0521/mason-null-ls.nvim",
-    })
+	use({
+		"jose-elias-alvarez/null-ls.nvim",
+		"jayp0521/mason-null-ls.nvim",
+	})
 
+	use("MunifTanjim/prettier.nvim")
 end)
